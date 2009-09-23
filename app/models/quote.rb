b71487a6cb4e91abf6dceeb194b550851d5db17d
Paladin <support@paladinweb.net>
2009-09-23 16:46:16 -0500

    Moved Randomization into model with slightly improved performance

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,13 @@
 class Quote < ActiveRecord::Base
   validates_presence_of :author, :message => 'required'
   validates_presence_of :quote, :message => 'required'
+  
+  def self.random( author )
+	if( author.blank? )
+	  ids = find( :all, :select => 'id' )
+	else
+  	  ids = find( :all, :select => 'id', :conditions => [ 'author = ? ', author ] )
+  	end
+  	find(ids[rand(ids.length)]["id"].to_i) unless ids.blank?
+  end
 end
